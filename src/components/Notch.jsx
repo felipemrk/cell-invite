3a5b924bc4a839bc1d9c174d18784226ac55bec2
Felps <jfelipesmarques@gmail.com>
2026-08-16 00:26:24 -0300

    adicionei notch e centralizei

---
+++ src/components/Notch.jsx
@@ -0,0 +1,5 @@
+export default function Notch() {
+	return (
+		<div className="w-40 h-8 bg-black border-4 border-solid rounded-xl"></div>
+	);
+}
--- src/components/PhoneLayout.jsx
+++ src/components/PhoneLayout.jsx
@@ -1,7 +1,17 @@
+import wallpaper from "../assets/iphone_wllpp.jpg";
+import Notch from "./Notch";
+
 export default function Phone() {
-  return (
-    <div className="flex bg-black border-4 border-solid rounded-xl border-blue-500 text-red-500 w-[390px] h-[844px]">
-      Teste
-    </div>
-  );
+	return (
+		<div
+			className="flex border-4 border-solid rounded-xl border-white w-[390px] h-[844px] bg-cover bg-center"
+			style={{
+				backgroundImage: `url(${wallpaper})`,
+			}}
+		>
+			<header className="flex w-[390px] h-8 justify-center">
+				<Notch />
+			</header>
+		</div>
+	);
 }
